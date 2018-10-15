40521bac5ea35d1446157bf6f5ee343ad712ac72
bava18 <44151303+bava18@users.noreply.github.com>
2018-10-15 11:37:27 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,10 +3,6 @@ public class Hello{
         System.out.println("Hello World");
         System.out.println("Free T-shirts");
         System.out.println("Hopefully We all get it :P");
-        System.out.println("Test 1");
-        System.out.println("Test 2");
-        System.out.println("Test 3");
-        System.out.println("Test 4");
-        System.out.println("Test 5");
+        
     }
 }
